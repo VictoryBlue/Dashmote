6d3befee54ebde27b2dffa2a8e45681cd8347a7a
VictoryBlue <88573273@qq.com>
2023-10-14 17:57:52 +0800

    bugfix: search by name support insensitive alphabet

--- src/components/dashboad/DmDashboad.jsx
+++ src/components/dashboad/DmDashboad.jsx
@@ -8,31 +8,36 @@ const DmDashBoard = ({ name }) => {
   const [projects, setProjects] = useState(devData);
   const onDelete = (key) => {
     // to simplify the logic
-    setProjects(projects.filter((project) => project.id !== key));
+    setProjects(devData.filter((project) => project.id !== key));
   };
   const onSearch = (query) => {
-    setProjects(devData.filter((project) => project.name.includes(query)));
+    setProjects(
+      devData.filter((project) =>
+        project.name.toLowerCase().includes(query.toLowerCase())
+      )
+    );
   };
   return (
     <>
       <div className="dashboard-container">
         <p className="welcome-info">{`Hello ${name.split(' ')[0]}!`}</p>
         <DmInput onSearch={onSearch} />
-
-        <Flex gap={'8px'} vertical="true" className="table-body">
-          {/* if projects are exsits then render rows else render a message to inform client  */}
-          {projects.length > 0 ? (
-            projects.map((data) => {
-              return <Row {...data} key={data.id} onClick={onDelete} />;
-            })
-          ) : (
-            <Alert
-              message="welcome to dashmote"
-              description="To create a new project"
-              type="info"
-            />
-          )}
-        </Flex>
+        <div className="table-body">
+          <Flex gap={'8px'} vertical="true">
+            {/* if projects are exsits then render rows else render a message to inform client  */}
+            {projects.length > 0 ? (
+              projects.map((data) => {
+                return <Row {...data} key={data.id} onClick={onDelete} />;
+              })
+            ) : (
+              <Alert
+                message="welcome to dashmote"
+                description="To create a new project"
+                type="info"
+              />
+            )}
+          </Flex>
+        </div>
       </div>
     </>
   );
