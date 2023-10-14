7a75e02038f056f8157364e5ae5a7da03301463a
VictoryBlue <88573273@qq.com>
2023-10-14 16:00:26 +0800

    style: Input to fit mobile devices, change the dom to keep the layout stable

--- src/components/DmInput/DmInput.jsx
+++ src/components/DmInput/DmInput.jsx
@@ -5,20 +5,27 @@ const { Search } = Input;
 const DmInput = ({ onSearch }) => {
   // feat 4 As a user I want to search my projects by name
   return (
-    <div className="DmInput">
-      <Search
-        placeholder="input search text"
-        onSearch={(e) => {
-          onSearch(e);
-        }}
-        style={{
-          width: 200,
-          position: 'absolute',
-          bottom: '8px',
-          right: '8px',
-        }}
-      />
-    </div>
+    <>
+      <div className="Dm-input">
+        <p className="search-info">
+          Here you can find your projects and dashboards.
+        </p>
+        <div>
+          <Search
+            placeholder="input search text"
+            onSearch={(e) => {
+              onSearch(e);
+            }}
+            style={{
+              width: 200,
+              position: 'absolute',
+              bottom: '8px',
+              right: '8px',
+            }}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
