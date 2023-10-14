33a5bed4cd20ebdf02fb85eeddf1ca88f472a653
VictoryBlue <88573273@qq.com>
2023-10-14 16:04:21 +0800

    refactor: consider maintainability, provide components with uniform information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import DmHeader from './components/DmHeader';
+import DmHeader from './components/DmHeader/DmHeader.jsx';
+import DmDashBoard from './components/dashboad/DmDashboad.jsx';
 import './App.css';
 import React from 'react';
 
 function App() {
+  const name = 'Sarah green';
   return (
     <div className="App">
-      <DmHeader className="header" />
+      <DmHeader className="header" name={name} />
+      <DmDashBoard name={name} />
     </div>
   );
 }
