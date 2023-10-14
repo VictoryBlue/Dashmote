13004ccc67900a4ce2249f01040bc4943ee61106
VictoryBlue <88573273@qq.com>
2023-10-14 14:09:12 +0800

    resource: change the test data

---
+++ src/data/devData.js
@@ -0,0 +1,13 @@
+const data = [
+  { id: 1, name: 'Project A', users: 4, dashboards: 3, category: 'D' },
+  { id: 2, name: 'Project B', users: 2, dashboards: 4, category: 'C' },
+  { id: 3, name: 'Project C', users: 1, dashboards: 2, category: 'F' },
+  { id: 4, name: 'Project D', users: 3, dashboards: 2, category: 'D' },
+  { id: 5, name: 'Project D', users: 3, dashboards: 2, category: 'D' },
+  { id: 6, name: 'Project D', users: 3, dashboards: 2, category: 'D' },
+  { id: 7, name: 'Project D', users: 3, dashboards: 2, category: 'D' },
+  { id: 8, name: 'Project D', users: 3, dashboards: 2, category: 'D' },
+  { id: 9, name: 'Project D', users: 3, dashboards: 2, category: 'D' },
+];
+
+export default data;
